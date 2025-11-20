53a67d03e644b21f22febab11a5e0474cae016d6
Evariste Nkurunziza <nkurunzizaevariste060@gmail.com>
2025-11-20 17:16:48 +0200

    Refacter scramble function which check if there character in first string which can be arranged to form the second one

--- 5kyu/scrambles.js
+++ 5kyu/scrambles.js
@@ -3,11 +3,11 @@ const scramble = (str1, str2)=> {
   const freq = str1.split('').reduce((acc, char) => {
     acc[char] = (acc[char] || 0) + 1;
     return acc;
-  }, {});
+  }, {});//creating the object of the character and it's frequency in first string
 
   return str2.split('').every(char => {
-    if (!freq[char]) return false;
-    freq[char]--;
+    if (!freq[char]) return false; // if the  string 2 does not include the character return false
+    freq[char]--; //decreament the value of the character we have found in the string 2 currently
     return true;
   });
 }
