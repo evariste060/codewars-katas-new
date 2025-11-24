8074828d041a24d4c42b0903ab8b4dd0419dc450
Evariste Nkurunziza <nkurunzizaevariste060@gmail.com>
2025-11-24 18:46:32 +0200

    add this kata to find the the number the character can be shifted to for the fist string

---
+++ 6qu/shifted.js
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/5596f6e9529e9ab6fb000014/train/javascript
+function shiftedDiff(first, second) {
+  if (first.length !== second.length) return -1;
+
+  const double = first + first; //double first string
+  const leftIndex = double.indexOf(second); //find the index of the second string in doubled string
+
+  if (leftIndex === -1) return -1;
+
+  // convert left-rotation index to right-rotation count
+  return leftIndex === 0 ? 0 : first.length - leftIndex;
+}
