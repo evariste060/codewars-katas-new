493b5e35c20500b89942d6857809a100ce866f5e
Evariste Nkurunziza <nkurunzizaevariste060@gmail.com>
2025-11-27 19:20:47 +0200

    add the static method promise.all that fetch at multiple urls

--- promise/fetch.js
+++ promise/fetch.js
@@ -7,18 +7,35 @@
 //         console.log(info)
 //     }
 // }).catch((err=>console.log(err)))
+//getting some data
+const list = document.getElementById('userList');
 async function getData(url){
     let response = await fetch(url);
     let data = await response.json()
     return data
 }
+//function display users
+function displayUser(users){
+    const list = document.getElementById('userList');
+    for(let user of users){
+        const li = document.createElement("li");
+        li.innerHTML= `<strong>Name: ${user.name}</strong><br><strong>Email </strong>${user.email} <br> <strong>Address </strong>${user.address.city}`;
+        list.appendChild(li)
+    }
+}
 getData("https://jsonplaceholder.typicode.com/users")
-.then(result=>{
-    console.log(result)
-    // for(let key of result){
-    //     const personalInfo = Object.values(key);
-    //     console.log(personalInfo)
-    // }
+.then(result=>displayUser(result))
+// .catch(err=>console.log(err))
+const data = {
+    method: "POST",
+    headers: {"content-Type": "application/json"},
+    body: JSON.stringify({title: "Hello",body:"this is for testing"})
+}
+async function postData(url,data){
+    let response = await fetch(url,data);
+    return response;
+}
+postData("https://jsonplaceholder.typicode.com/users",data)
+.then(result=>console.log(result))
 
-})
-.catch(err=>console.log(err))
+
---
+++ promise/promiseAPI.js
@@ -0,0 +1,14 @@
+let urls = [
+  'https://api.github.com/users/iliakan',
+  'https://api.github.com/users/remy',
+  'https://api.github.com/users/jeresig'
+];
+
+// map every url to the promise of the fetch
+let requests = urls.map(url => fetch(url));
+
+// Promise.all waits until all jobs are resolved
+Promise.all(requests)
+.then(response=> console.log(response))
+
+  
