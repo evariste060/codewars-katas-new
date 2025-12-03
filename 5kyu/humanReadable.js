4084f41936078f929e965d935fd2a4a1d1447551
Evariste Nkurunziza <nkurunzizaevariste060@gmail.com>
2025-12-03 14:26:37 +0200

    Add the kata which change the second into human readable format hour:minus:sec

---
+++ 5kyu/humanReadable.js
@@ -0,0 +1,9 @@
+function humanReadable (seconds) {
+   
+  let hours = Math.floor(seconds/3600);
+  let remainingSeconds = seconds%3600;
+  let minutes = Math.floor(remainingSeconds/60)
+  let sec = remainingSeconds%60
+  return [hours,minutes,sec].map(e=>e.toString().padStart(2,'0')).join(':')
+}
+console.log(humanReadable(3600))// 01:00:00
