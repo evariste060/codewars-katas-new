71c88365f1f8580ec1925a9012553fe2d3ed0929
Evariste Nkurunziza <nkurunzizaevariste060@gmail.com>
2026-03-17 22:28:01 +0200

    Add the function which check if we have valid braces

---
+++ 6qu/validBraces.js
@@ -0,0 +1,22 @@
+function validBraces(braces) {
+  let stack = [];
+  let pairs = {
+    ')': '(',
+    ']': '[',
+    '}': '{'
+  };
+
+  for (let brace of braces) {
+    
+    if (brace === '(' || brace === '[' || brace === '{') {
+      stack.push(brace);
+    } 
+    else {
+      if (stack.pop() !== pairs[brace]) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+}
